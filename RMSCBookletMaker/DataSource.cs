62dfc99b2e9ae8e800e4cedef4e3ae353100cd55
j0hnds <dj0hnve@gmail.com>
2009-09-25 15:14:54 -0600

    Modified the data source to externalize the settings for the data base.

--- RMSCBookletMaker/DataSource.cs
+++ RMSCBookletMaker/DataSource.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Data;
 using Npgsql;
+using System.Configuration;
 
 namespace RMSCBookletMaker
 {
@@ -9,11 +10,11 @@ namespace RMSCBookletMaker
 	
 	public class DataSource
 	{
-		private const string HOST = "hagrid";
-		private const int PORT = 5432;
-		private const string USER = "postgres";
-		private const string PASSWORD = "jordan123";
-		private const string DB_NAME = "RMSC";
+		private static readonly string HOST = ConfigurationManager.AppSettings["host"];
+		private static readonly int PORT = int.Parse(ConfigurationManager.AppSettings["port"]);
+		private static readonly string USER = ConfigurationManager.AppSettings["user"];
+		private static readonly string PASSWORD = ConfigurationManager.AppSettings["password"];
+		private static readonly string DB_NAME = ConfigurationManager.AppSettings["dbname"];
 		private const string CONNECTION_TEMPLATE= "Server={0};Port={1};User Id={2};Password={3};Database={4}";
 		private static readonly string CONNECTION_STRING = string.Format(CONNECTION_TEMPLATE, HOST, PORT, USER, PASSWORD, DB_NAME);
 		
